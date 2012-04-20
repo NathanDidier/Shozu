e3e4f7df2fbab56719e5c080a4571c1bfd4a7fef
Thibault Jouan <tj+github@bricozor.com>
2012-04-20 14:25:39 +0000

    Improve ShozuTwigExtension usage and PHPDoc comments

--- Twig/ShozuTwigExtension.php
+++ Twig/ShozuTwigExtension.php
@@ -6,11 +6,19 @@ class ShozuTwigExtension extends \Twig_Extension
 {
     protected $shozu;
 
+    /**
+     * @param   \shozu\Shozu    $shozu  Shozu instance
+     */
     public function __construct($shozu)
     {
         $this->shozu = $shozu;
     }
 
+    /**
+     * Returns a list of global variables to add to the existing list.
+     *
+     * @return  array   An array of global variables
+     */
     public function getGlobals()
     {
         return array(
@@ -18,6 +26,11 @@ class ShozuTwigExtension extends \Twig_Extension
         );
     }
 
+    /**
+     * Returns the name of the extension.
+     *
+     * @return  string  The extension name
+     */
     public function getName()
     {
         return 'shozu';
