91d6b8b36ebf7d096d17e8b17fbba09cd5b022af
Thibault Jouan <tj+github@bricozor.com>
2012-04-20 14:38:36 +0000

    Implement action() helper for Twig views

--- Twig/ShozuTwigExtension.php
+++ Twig/ShozuTwigExtension.php
@@ -27,6 +27,18 @@ class ShozuTwigExtension extends \Twig_Extension
     }
 
     /**
+     * Returns a list of functions to add to the existing list.
+     *
+     * @return  array   An array of functions
+     */
+    public function getFunctions()
+    {
+        return array(
+            'action' => new \Twig_Function_Method($this, 'functionAction')
+        );
+    }
+
+    /**
      * Returns the name of the extension.
      *
      * @return  string  The extension name
@@ -34,5 +46,20 @@ class ShozuTwigExtension extends \Twig_Extension
     public function getName()
     {
         return 'shozu';
+    }
+
+    /**
+     * Return rendered view for requested action.
+     *
+     * @param   string  $action     Fully qualified action name
+     * @param   array   $params     Parameters for requested action
+     *
+     * @return  string  The rendered view
+     */
+    public function functionAction($action, $params = array())
+    {
+        list($app, $controller, $action) = explode('/', $action);
+
+        return \shozu\Dispatcher::render($app, $controller, $action, $params);
     }
 }
