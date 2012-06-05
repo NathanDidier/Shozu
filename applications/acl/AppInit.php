5e267d9cfad1e7c23746ae744f0c77f7783ecad7
Mickael Desfrenes <desfrenes@gmail.com>
2012-06-05 11:36:47 +0200

    remove applications dir

--- applications/acl/AppInit.php
+++
@@ -1,2 +0,0 @@
-<?php
-define('ACL_SUCCESSFUL_COOKIEAUTH_REDIRECT', \shozu\Shozu::getInstance()->url('welcome/index/index'));
--- applications/acl/views/welcome.php
+++
@@ -1,2 +0,0 @@
-<p>Welcome Home, <?php echo $this->escape($user->login); ?> !</p>
-<p><a href="<?php echo $redirect_url; ?>">Click here</a> to be redirected.</p>
