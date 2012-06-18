b6218f81cac425a6ca4e88acbe4b7fa79b022c0c
Thibault Jouan <tj+github@bricozor.com>
2012-06-18 21:21:36 +0000

    Fix underscore inflector for digits and acronyms

---
+++ test/unit/InflectorTest.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once __DIR__ . '/test_helper.php';
+
+require_once 'Inflector.php';
+
+use \Shozu\Inflector as Inflector;
+
+class InflectorTest extends PHPUnit_Framework_TestCase
+{
+    public function testUnderscoreLowerCaseFirstLetter()
+    {
+        $this->assertEquals(
+            'bill',
+            Inflector::underscore('Bill')
+        );
+    }
+
+    public function testUnderscoreLowerCaseUpcasedLettersAndAddUnderscore()
+    {
+        $this->assertEquals(
+            'bill_data',
+            Inflector::underscore('BillData')
+        );
+    }
+
+    public function testUnderscoreAddUnderscoreBeforeDigit()
+    {
+        $this->assertEquals(
+            'bill_data_4',
+            Inflector::underscore('BillData4')
+        );
+    }
+
+    public function testUnderscoreAddUnderscoreBeforeNumber()
+    {
+        $this->assertEquals(
+            'bill_data_42',
+            Inflector::underscore('BillData42')
+        );
+    }
+
+    public function testUnderscoreRespectAcronyms()
+    {
+        $this->assertEquals(
+            'get_html',
+            Inflector::underscore('getHTML')
+        );
+    }
+}
